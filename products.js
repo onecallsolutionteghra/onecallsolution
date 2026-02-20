5a6ab3eeddf3f2609d3f3c744e0baa5d57644b25
onecallsolutionteghra <onecallsolution.teghra@gmail.com>
2026-02-20 14:44:36 +0530

    Delete products.js

--- products.js
+++
@@ -1,5 +0,0 @@
-const products = [
-  {id:1,name:"Rice 5kg",price:250,cat:"Groceries",img:"https://images.unsplash.com/photo-1586201375761-83865001e31c"},
-  {id:2,name:"Cooking Oil",price:120,cat:"Groceries",img:"https://images.unsplash.com/photo-1604908176997-125f25cc6f3d"},
-  {id:3,name:"Plumbing Service",price:499,cat:"Services",img:"https://images.unsplash.com/photo-1581091870627-3f6b0a8d60e1"}
-];
